80f4fad076ba31d242c4df5c3cf7d9cb901cdebb
Jackson Silva Lopes <j.a.ckson@hotmail.com>
2020-04-02 18:25:32 -0300

    Inicialização do projeto

---
+++ src/main/java/com/couse/mvc/DemoMvcApplication.java
@@ -0,0 +1,13 @@
+package com.couse.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMvcApplication.class, args);
+	}
+
+}
